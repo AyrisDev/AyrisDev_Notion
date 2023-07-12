061579512bb880a865cbc74c5a895e6a09bcb75e
mstfyldz <mstfyldz@mstfyldz-MacBook-Pro.local>
2023-07-12 21:30:18 +0300

    b

--- components/Home/things.js
+++ components/Home/things.js
@@ -6,7 +6,7 @@ import Stacks from '@/hooks/things.json'
 const Things = () => {
   return (
     <div className='relative my-20'>
-      <TitleCard titles='Things I Used' allTitles='Look All' />
+      <TitleCard titles='Stack' allTitles='Look All' />
 
       <div className='w-full grid grid-cols-9 mt-4 gap-4 items-center'>
         {Stacks.map((stacks) => (
--- components/ui/tip.js
+++ components/ui/tip.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Image from 'next/image'
 const tip = ({ stacks }) => {
   return (
     <div className='group relative '>
-      <div className='flex flex-row hover:opacity-80 cursor-pointer border bg-gray-300 hover:bg-gray-700 rounded-xl'>
-        <img src={stacks.imgLink} className='rounded-full  w-12 h-12  p-1' />
-        <div className='flex justify-center items-center text-gray-500'>
+      <div className='flex flex-row group:hover:opacity-80 cursor-pointer   hover:bg-gray-300 rounded-xl'>
+        <img
+          src={stacks.imgLink}
+          className='rounded-full  w-12 h-12  p-1 hover:opacity-80'
+        />
+        <div className='flex justify-center items-center hover:text-gray-200'>
           {stacks.title}
         </div>
       </div>
